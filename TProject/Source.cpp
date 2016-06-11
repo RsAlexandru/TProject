cc16b7107e8495be9cf1645b3ff495d4a7c85102
RsAlexandr <raresvrm@yahoo.com>
2016-06-11 18:14:11 +0300

    Traducerea

--- TProject/Source.cpp
+++ TProject/Source.cpp
@@ -156,6 +156,112 @@ struct node* TreeFromFileEn(FILE *f)
 	return ptree;
 }
 
+struct node *splayro(struct node *root, char *key)
+{
+	node N, *l, *r, *y;
+	if (root == NULL) return root;
+	N.left = N.right = NULL;
+	l = r = &N;
+
+	for (;;)
+	{
+		if (strcmp(key, root->word)<0)
+		{
+			if (root->left != NULL && strcmp(key, root->left->word)<0)
+			{
+				y = root->left;
+				root->left = y->right;
+				y->right = root;
+				root = y;
+			}
+			if (root->left == NULL)
+				break;
+			r->left = root;
+			r = root;
+			root = root->left;
+		}
+		else if (strcmp(key, root->word)>0)
+		{
+			if (root->right != NULL && strcmp(key, root->right->word)>0)
+			{
+				y = root->right;
+				root->right = y->left;
+				y->left = root;
+				root = y;
+			}
+			if (root->right == NULL)
+				break;
+			l->right = root;
+			l = root;
+			root = root->right;
+		}
+		else break;
+	}
+
+	l->right = root->left;
+	root->left = root->right;
+	root->left = N.right;
+	root->right = N.left;
+	return root;
+}
+
+struct node *splayen(struct node *root, char *key)
+{
+	node N, *l, *r, *y;
+	if (root == NULL) return root;
+	N.left = N.right = NULL;
+	l = r = &N;
+
+	for (;;)
+	{
+		if (strcmp(key, root->meaning)<0)
+		{
+			if (root->left != NULL && strcmp(key, root->left->meaning)<0)
+			{
+				y = root->left;
+				root->left = y->right;
+				y->right = root;
+				root = y;
+			}
+			if (root->left == NULL) break;
+			r->left = root;
+			r = root;
+			root = root->left;
+		}
+		else if (strcmp(key, root->meaning)>0)
+		{
+			if (root->right != NULL && strcmp(key, root->right->meaning)>0)
+			{
+				y = root->right;
+				root->right = y->left;
+				y->left = root;
+				root = y;
+			}
+			if (root->right == NULL) break;
+			l->right = root;
+			l = root;
+			root = root->right;
+		}
+		else break;
+	}
+
+	l->right = root->left;
+	root->left = root->right;
+	root->left = N.right;
+	root->right = N.left;
+	return root;
+}
+
+struct node *searchro(struct node *root, char *word)
+{
+	return splayro(root, word);
+}
+
+struct node*searchen(struct node *root, char *meaning)
+{
+	return splayen(root, meaning);
+}
+
 
 BOOL CALLBACK DlgDespre(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK DlgDictionarRo(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam);
@@ -333,6 +439,58 @@ BOOL CALLBACK DlgDictionarRo(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lPar
 		}
 		break;
 
+		case WM_COMMAND:
+			switch (LOWORD(wParam))
+			{
+				case IDC_ADD_STRING:
+				{
+					node *tree_root = TreeFromFile(f);
+					int i, n = 0;
+					char *p, v[100][100], s[100], string[100] = "";
+					int len = GetWindowTextLength(GetDlgItem(hwnd, IDC_TEXT1));
+					if (len > 0)
+					{
+						char* buf;
+						buf = (char*)malloc(len + 1);
+
+						GetDlgItemText(hwnd, IDC_TEXT1, buf, len + 1);
+
+						p = strtok(buf, " ");
+
+						while (p != NULL)
+						{
+							strcpy(v[n], p);
+							n++;
+							p = strtok(NULL, " ");
+						}
+
+						for (i = 0; i<n; i++)
+						{
+							node* temp = splayro(tree_root, v[i]);
+							tree_root = TreeFromFile(f);
+
+							if (strcmp(v[i], temp->word) == 0)
+							{
+								strcat(string, temp->meaning);
+								strcat(string, " ");
+							}
+						}
+
+						SetDlgItemText(hwnd, IDC_TEXT2, string);
+					}
+			    }
+				break;
+
+				case IDC_CLEAR1:
+				{
+					free(tree_root);
+					SetDlgItemText(hwnd, IDC_TEXT1, "");
+					SetDlgItemText(hwnd, IDC_TEXT2, "");
+				}
+				break;
+			}
+		break;
+
 		case WM_CLOSE:
 		EndDialog(hwnd, 0);
 		break;
@@ -358,6 +516,58 @@ BOOL CALLBACK DlgDictionarEn(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lPar
 		MoveToEx(hDC, 0, 212, NULL);
 		LineTo(hDC, 700, 212); 
 		}
+		break;
+
+		case WM_COMMAND:
+			switch (LOWORD(wParam))
+			{
+				case IDC_ADD_STRING1:
+				{
+					node *tree_root = TreeFromFileEn(f);
+					int i, n = 0;
+					char *p, v[100][100], s[100], string[100] = "";
+					int len = GetWindowTextLength(GetDlgItem(hwnd, IDC_TEXT4));
+					if (len > 0)
+					{
+						char* buf;
+						buf = (char*)malloc(len + 1);
+
+						GetDlgItemText(hwnd, IDC_TEXT4, buf, len + 1);
+
+						p = strtok(buf, " ");
+
+						while (p != NULL)
+						{
+							strcpy(v[n], p);
+							n++;
+							p = strtok(NULL, " ");
+						}
+
+						for (i = 0; i < n; i++)
+						{
+							node* temp = splayen(tree_root, v[i]);
+							tree_root = TreeFromFileEn(f);
+
+							if (strcmp(v[i], temp->meaning) == 0)
+							{
+								strcat(string, temp->word);
+								strcat(string, " ");
+							}
+						}
+
+						SetDlgItemText(hwnd, IDC_TEXT3, string);
+					}
+				}
+			break;
+
+			case IDC_CLEAR1:
+			{
+				free(tree_root);
+				SetDlgItemText(hwnd, IDC_TEXT3, "");
+				SetDlgItemText(hwnd, IDC_TEXT4, "");
+			}
+			break;
+			}
 		break;
 
 		case WM_CLOSE:
